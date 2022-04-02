f73dd6ed8bf5729cb58861a257cb799edfcfa102
burak karacakaya <burakkaracakaya@yahoo.com>
2022-04-02 08:37:47 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself
+            string kategoriEtiketi = "Kategoriler"; //kategoriEtiketi = Değer tutucu, alias, etiket
+            int ogrenciSayısı = 3200;
+            double notOrtalaması = 2.55;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu göster");
+
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+
+
+            }
+
+
+
+            if (sistemeGirisYapmisMi == true)
+                {
+                    Console.WriteLine("Kullanıcı ayarları butonu");
+
+                }
+                else
+                {
+                    Console.WriteLine("Giriş yap butonu");
+
+                }
+            
+
+
+
+        Console.WriteLine(kategoriEtiketi);
+
+
+
+
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Matematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+
+
+            dortIslem.Topla(7, 9);
+
+
+            
+        }
+    }
+}
